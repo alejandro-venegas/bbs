854089beb50c9f625e1b61a97ccfcd3c7ae22fd7
Alejandro Venegas <avenegasc14@gmail.com>
2020-05-08 13:20:04 -0600

    Left nav basic styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReportesComponent } from './reportes/reportes.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'reportes', pathMatch: 'full' },
+  { path: 'reportes', component: ReportesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
