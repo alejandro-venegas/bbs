7c5475885c7304cf2ca04bda4a52e4a0f93faf94
Alejandro Venegas <avenegasc14@gmail.com>
2020-05-08 11:32:23 -0600

    Add basic layout components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ClientApp';
+  baseUrl = environment.base_url;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get( this.baseUrl + '/weatherforecast').subscribe(res => console.log(res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReportesComponent } from './reportes/reportes.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportesComponent,
+    HeaderComponent,
+    LeftNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
